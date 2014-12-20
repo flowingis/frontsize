95b6eeef3debfee72b06769b6597f0d456da48a6
Alessandro Minoccheri <alessandro@webanddesign.it>
2014-12-20 17:07:53 +0100

    fix path in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
         compileFileTest : 'compile-test.scss',
         themeName       : 'default',
         themeImg        : 'themes/default/img/',
-        path            : 'test/',
+        path            : 'test',
         testCss         : '<%= path %>/frontsize.test.css',
         autoprefixerCss : '<%= path %>/frontsize.autoprefixer.min.css',
         minifiedCss     : '<%= path %>/frontsize.min.css',
