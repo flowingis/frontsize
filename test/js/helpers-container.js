2ce58f544a97af617490342a69f25143df6727ef
Vittorio Vittori <me@vittoriovittori.com>
2016-11-30 18:20:24 +0100

    Adds mixin has-parent

--- test/js/helpers-container.js
+++ test/js/helpers-container.js
@@ -2,6 +2,34 @@ var helper = require('../helper');
 
 helper.test({
 expect:
+`.helpers-container-child {
+  @include has-parent('parent-element') {
+    content: 'case 1';
+  }
+  @include has-parent('.parent-element') {
+    content: 'case 2';
+  }
+  @include has-parent('#parent-element') {
+    content: 'case 3';
+  }
+}`,
+toBe:
+`
+.parent-element .helpers-container-child {
+  content: 'case 1';
+}
+
+.parent-element .helpers-container-child {
+  content: 'case 2';
+}
+
+#parent-element .helpers-container-child {
+  content: 'case 3';
+}`
+});
+
+helper.test({
+expect:
 `.helpers-container {
   @include limit((
     '>=desktop-small': 1280px,
