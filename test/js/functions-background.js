92ce29fa7dc16b37e9e4bacb0d65c7cee6c11dbc
Vittorio Vittori <me@vittoriovittori.com>
2017-02-20 19:10:25 +0100

    Updates docs

--- test/js/functions-background.js
+++ test/js/functions-background.js
@@ -13,11 +13,35 @@ toBe:
 
 helper.test({
 expect:
-`.background-image-theme {
+`.background-vertical-gradient {
+  background-image: vertical-gradient(#ff724d, #33f2fd);
+}`,
+toBe:
+`.background-vertical-gradient {
+  background-image: linear-gradient("top, #ff724d 0%, #33f2fd 100%");
+}`
+});
+
+helper.test({
+expect:
+`$path-images: 'path/to/images/';
+.background-image-theme {
   background-image: theme('image.svg');
 }`,
 toBe:
 `.background-image-theme {
-  background-image: url("production/img/theme/img/image.svg");
+  background-image: url("path/to/images/image.svg");
+}`
+});
+
+helper.test({
+expect:
+`$path-images: 'path/to/images/';
+.background-image-theme {
+  background-image: theme('image.svg', 'http://cdn.images.com/');
+}`,
+toBe:
+`.background-image-theme {
+  background-image: url("http://cdn.images.com/image.svg");
 }`
 });
